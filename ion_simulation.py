6219a9774ce5c45d10256686bc74f81922ca827c
itamareyal <itamareyal93@gmail.com>
2020-12-28 08:46:21 +0200

    deriv not working, maybe switch to sin

--- ion_simulation.py
+++ ion_simulation.py
@@ -7,19 +7,37 @@ ion_simulation.py
 ----------------------------------------------------------------------'''
 import numpy as np
 import matplotlib.pyplot as plt
-
 from scipy.misc import derivative
-import progressbar
-from datetime import datetime
-import os
 import random
 
-from ion_parser import *
 #from rims import *
+'''----------------------------------------------------------------------
+                                DEFINES
+----------------------------------------------------------------------'''
+diffusion_coefficient_dict = {
+    # Deff dictionary. [m^2/sec]
+    "Lead Pb+2"          : 0.945,
+    "Potassium K+"     : 1.960,
+    "Calcium Ca+2"       : 0.793,
+    "Sodium Na+"        : 1.330
+}
 
+typical_ion_speed = {
+    # speed dictionary. [m^2/(V*sec)]
+    "Lead Pb+2": 5.19 * pow(10,-8),
+    "Potassium K+": 7.62 * pow(10,-8),
+    "Calcium Ca+2": 5.19 * pow(10,-8),
+    "Sodium Na+": 5.19 * pow(10,-8)
+}
 
+INTERVALS_FLASH_RATIO = 10
+BOLTZMANN_CONSTANT = 1.38064852 * pow(10,-23)
+AVOGADRO_NUMBER = 6.0221409 * pow(np.e,23)
+TEMPERATURE = 298
 
-
+'''----------------------------------------------------------------------
+                            IMPLEMENTATIONS
+----------------------------------------------------------------------'''
 
 def get_potential_profile(a1, a2, L, x):
     # format of elecrtron experiment, units of Vq
@@ -29,39 +47,85 @@ def create_arena_E():
     x=np.linspace(0,0.8) # micrometer
     return plt.plot(x,get_potential_profile(-0.25, -0.05, 0.8, x))
 
-
-    
-
-
-
 class ion:
-    def __init__(self, element):
-        
+    def __init__(self, ion, potential_profile, flash_frequency, flash_mode):
 
-        #self.loc = random.uniform(0,np.pi)
-        #self.speed = random.uniform(0,np.pi)
+        self.ion = ion
+        self.diffusion = diffusion_coefficient_dict[ion]
+        self.potential_profile_list= potential_profile
+        self.flash_frequency = flash_frequency
+        self.flash_period = 1 / flash_frequency
+        self.flash_mode = flash_mode
 
-        self.mass = 6
+        self.L = potential_profile[0] + potential_profile[1]
+        self.loc = random.uniform(0,self.L)
+        self.speed = random.uniform(-typical_ion_speed[ion],typical_ion_speed[ion])
 
-
-        self.intervals = 1
         self.intervals_count = 0
         self.points = 100
-        self.inarena = 0
+        self.arena_count = 0
         self.loc_hist = 0
 
-    def create_arena(self,a,b,A):
-        x1=np.linspace(0,a) # micrometer
-        x2=np.linspace(a,b)
-        x=np.linspace(0, a+b)
+    def get_intervals(self):
+        self.interval = self.flash_period / INTERVALS_FLASH_RATIO
+
+    def get_gamma(self):
+        self.gamma = BOLTZMANN_CONSTANT * TEMPERATURE / self.diffusion
+
+    def electric_force(self, x):
+        electric_field = derivative(self.arena * ion.ratchet_mode(self), x, dx=1e-3)
+        return - self.gamma * electric_field
+
+    def noise(self):
+        var = np.divide(1,np.sqrt(4 * np.pi * self.diffusion * self.interval))
+        ksai = np.random.normal(0, np.power(var,2))
+        return np.multiply(ksai,np.sqrt(2 * self.diffusion * self.interval))
+
+    def create_arena(self):
+        a = self.potential_profile_list[0]
+        b = self.potential_profile_list[1]
+        A = self.potential_profile_list[2]
+        x = np.linspace(0, a+b)
         f1=A * np.divide(x,a)
         f2=A * np.divide((x-(a+b)),(-b))
         step = 0.5*(np.sign(x-a) +1)
         f=  f1 -step*f1 + step* f2
-        plt.plot(x, f)
-        plt.xlabel("X [um]")
-        plt.ylabel("V [v]")
+        self.arena = f
+        # plt.plot(x, f)
+        # plt.xlabel("X [um]")
+        # plt.ylabel("V [v]")
 
+    def ratchet_mode(self):
+        mode = np.divide(self.intervals_count ,self.flash_period)
+        if mode % 2:
+            return self.flash_mode
+        else:
+            return 1
+
+    def get_new_x(self):
+        xt = self.loc
+        new_x = xt + np.multiply(self.electric_force(xt), self.interval) + self.noise
+
+        while new_x > self.L:
+            new_x -= self.L
+            self.arena_count += 1
+
+        while new_x < self.L:
+            new_x += self.L
+            self.arena_count -= 1
+
+        return new_x
+
+    def simulate_ion(self):
+        ion.create_arena(self)
+        new_x =0
+        while self.intervals_count <= self.points:
+
+            new_x = ion.get_new_x(self)
+            self.loc = new_x
+            self.intervals_count += 1
+
+        return self.L * self.arena_count + new_x
 
 
     #def get_max_x0(self, ):
--- rims.py
+++ rims.py
@@ -24,19 +24,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from scipy.constants import k
+import scipy.stats as st
 import progressbar
 from datetime import datetime
 import os
 import random
-from ion_parser import *
+#from ion_parser import *
 from ion_simulation import *
+
 '''----------------------------------------------------------------------
                                 DEFINES
 ----------------------------------------------------------------------'''
-ELECREON_MODE = False
-TEMPERATURE = 298           # T in kelvin
-Kb = scipy.constants.k      # Boltzman constant
-INTERVAL = 0.1
+
+ION_LIST = ["Lead Pb+2", "Potassium K+", "Calcium Ca+2", "Sodium Na+"]
+FLASHING_MODES = [0,-1]
+
 
 '''----------------------------------------------------------------------
                                EXECUTION
@@ -51,37 +53,36 @@ print("-------------------------------------------------------")
 
 input("\npress ENTER to begin...\n")
 
-if ELECREON_MODE:
-    print("In ELECTRON MODE, the simulated particles are elecreons.")
 
-    ions_dict_list = [{
-        'symbol': 'e',
-        'chrage': periodictable.constants.electron_volt,
-        'quantity': 1
-        }]
-    print(ions_dict_list)
+print("-------------------------------------------------------")
+print("             Step 1- Configure the system")
+print("-------------------------------------------------------")
 
+# print("Please specify the ions in the system and their quantities.")
+# print("List ions in the following format: symbol charge mols\nSeperate ions by comma and press ENTER: \n")
+# print("\nFor example: \tH +1 2, Ag +2 5\n")
+#
+# ions_verified = False
+#
+# while ions_verified == False:
+#     ion_str = input("Enter ions: ")
+#     if ion_str == '':
+#         continue
+#     ions_dict_list = ion_inputs_to_attributes(ion_str)
+#     ions_verified = verify_ions(ions_dict_list)
+#
+# print_ions(ions_dict_list)
 
-else:
+print("\nSelect an ion to be simulated from the following list:")
+print("\t1) Lead Pb+2")
+print("\t2) Potassium K+")
+print("\t3) Calcium Ca+2")
+print("\t4) Sodium Na+")
+number_selection = int(input("Enter your selection:"))
+ion_selection = ION_LIST[number_selection]
 
-    print("-------------------------------------------------------")
-    print("             Step 1- Configure the system")
-    print("-------------------------------------------------------")
-
-    print("Please specify the ions in the system and their quantities.")
-    print("List ions in the following format: symbol charge mols\nSeperate ions by comma and press ENTER: \n")
-    print("\nFor example: \tH +1 2, Ag +2 5\n")
-
-    ions_verified = False
-
-    while ions_verified == False:
-        ion_str = input("Enter ions: ")
-        if ion_str == '':
-            continue
-        ions_dict_list = ion_inputs_to_attributes(ion_str)
-        ions_verified = verify_ions(ions_dict_list)
-
-    print_ions(ions_dict_list)
+print("\nIon selected: "+ion_selection)
+input("Press Enter to confirm...")
 
 print("-------------------------------------------------------")
 print("             Step 2- Configure the ratchet")
@@ -97,27 +98,70 @@ print("                               ")
 print("  \______/\____/               ")
 print("    a[um]   b[um]            \n")
 
-a = float(input("a[um] = "))
-b = float(input("b[um] = "))
-A = float(input("A[v] = "))
+a = float(input("\ta[um] = "))
+b = float(input("\tb[um] = "))
+A = float(input("\tA[v] = "))
 
-i = ion(1)
-i.create_arena(a,b,A)
 
-preview_potential = input("\nType y to see a preview of 1 period of the ratchet, Otherwise press ENTER to continue...")
-if preview_potential == 'y':
-    print("\nThe program will continue when you close the preview graph...")
-    plt.show()
 print("\nEnter ratchet flashing frequency in KHz:")
-potential_frequency = int(input("Ratchet frequency [KHz] = "))
-
+flash_frequency = int(input("Ratchet frequency [KHz] = "))
+print("\nEnter flashing mode number:\n\t1)ON/OFF\n\t2)+/-\n")
+flash_number = int(input("Flashing mode = "))
+flash_mode = FLASHING_MODES[flash_number]
+flash_period = 1 / flash_frequency
+potential_profile = [a,b,A]
 
 
 
 class rims:
-    def __init__(self, ions_dict_list, potential_profile):
-        self.ions = ions_dict_list
+    def __init__(self, ion, potential_profile, flash_frequency, flash_mode):
+        self.ion = ion
         self.potential_profile = potential_profile
+        self.flash_frequency = flash_frequency
+        self.flash_mode = flash_mode
+        self.number_of_simulations = 100
+
+    def create_ion(self):
+        self.start_time = datetime.now()
+        print(self.ion)
+        i = ion(self.ion, self.potential_profile, self.flash_frequency, flash_mode)
+        i.create_arena()
+        i.get_intervals()
+        i.get_gamma()
+        print("\nIon profile for simulation created.")
+        return i
+
+    def create_histogram(self, ion):
+        print("\nRIMS simulation in progress...")
+
+        bar = progressbar.ProgressBar(maxval=self.number_of_simulations,
+        widgets=[progressbar.Bar('=', '\t[', ']'), ' ', progressbar.Percentage()])
+        bar.start()
+        x_results = []
+        simulation_count =0
+        while simulation_count < self.number_of_simulations:
+            x_results.append(ion.simulate_ion())
+            simulation_count += 1
+            bar.update(simulation_count)
+        print("\nDone!")
+        print("\nPlotting results...")
+        plt.hist(x_results, density=True, bins=30, label="Data")
+        mn, mx = plt.xlim()
+        plt.xlim(mn, mx)
+        kde_xs = np.linspace(mn, mx, 301)
+        kde = st.gaussian_kde(x_results)
+        plt.plot(kde_xs, kde.pdf(kde_xs), label="PDF")
+        plt.legend(loc="upper left")
+        plt.ylabel('Probability')
+        plt.xlabel('Data')
+        plt.title("Histogram")
+        print("\ndone.")
+        plt.show()
+
+
+r= rims(ion_selection, potential_profile, flash_frequency, flash_mode)
+
+r.create_histogram(r.create_ion())
 
         
 
